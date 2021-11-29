a3a2c3bd06630cf8c4b7821da912ca7f6e9b08a4
Maya Comfort <mdcomfort@gmail.com>
2021-11-29 00:44:29 -0500

    letters/alphabet as a string approach for pangram function, still bad

--- functions.js
+++ functions.js
@@ -56,38 +56,12 @@ console.log(uniqueCharacters("Sets"))
 */
 
 function isPangram(sentence) {
-    let letters = {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: 0,
-        g: 0,
-        h: 0,
-        i: 0,
-        j: 0,
-        k: 0,
-        l: 0,
-        m: 0,
-        n: 0,
-        o: 0,
-        p: 0,
-        q: 0,
-        r: 0,
-        s: 0,
-        t: 0,
-        u: 0,
-        v: 0,
-        w: 0,
-        x: 0,
-        y: 0,
-        z: 0,
-      }
-
-      sentence.toLowerCase()
-      let splitSentence = sentence.split('')
-
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    sentence.toLowerCase()
+    let splitSentence = sentence.split('')
+    for (let i = 0; i < alphabet.length; i++) {
+        console.log[alphabet[i]]
+    }
 }
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog!"))
